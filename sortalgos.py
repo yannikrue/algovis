a858536839bb40b7bb98baee424b30e64c8870f3
yannikrue <96543444+yannikrue@users.noreply.github.com>
2022-08-29 13:58:41 +0200

    bubble sort

---
+++ sortalgos.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as pyplot
+import numpy as np
+
+amount = 15
+
+lst = np.random.randint(0, 100, amount)
+x = np.arange(0, amount, 1)
+
+n = len(lst)
+for i in range(n):
+    for j in range(0, n-i-1):
+        plt.bar(x, lst)
+        plt.pause(0.01)
+        plt.clf()
+        if lst[j] > lst[j+1]:
+            lst[j], lst[j+1] = lst[j+1], lst[j]
+plt.show()
